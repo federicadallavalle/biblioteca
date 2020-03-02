63218a083b686bb257ee7af0b25a94f1a6f332d4
Alex <gvnlsn@gmail.com>
2020-03-02 14:08:05 +0100

    Auto stash before merge of "master" and "origin/master"

--- biblioteca/src/model/dao/UtenteDao.java
+++ biblioteca/src/model/dao/UtenteDao.java
@@ -14,21 +14,30 @@ import utilities.Eccezione;
 
 public class UtenteDao {
 
-	public List<Utente> cercaUtente() throws Eccezione {
+	public List<Utente> cercaUtente(Utente ut) throws Eccezione {
 		ArrayList<Utente> lista = new ArrayList<>();
 		Connection conn = getConnection();
 		String sql = "SELECT * FROM biblioteca.utente "
-				+ "WHERE nome LIKE '%?%', cognome LIKE '%?%', email LIKE '%?%' ,"
+				+ "WHERE nome LIKE '%?%', cognome LIKE '%?%', email LIKE '%?%',"
 				+ "ruolo LIKE '%?%', username LIKE '%?%'";
 		PreparedStatement ps = null;
 		try {
 			ps = conn.prepareStatement(sql);
+            ps.setString(1, ut.getNome());
+            ps.setString(2, ut.getCognome());
+            ps.setString(3, ut.getEmail());
+            ps.setString(4, ut.getRuolo());
+            ps.setString(5, ut.getUsername());
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				
-
+				Utente utente = new Utente();
+				utente.setNome(rs.getString("nome"));
+				utente.setCognome(rs.getString("cognome"));
+				utente.setEmail(rs.getString("email"));
+				utente.setRuolo(rs.getString("ruolo"));
+				utente.setUsername(rs.getString("username"));
+				lista.add(utente);
 			}
-
 			ps.close();
 			conn.close();
 		} catch (SQLException e) {
