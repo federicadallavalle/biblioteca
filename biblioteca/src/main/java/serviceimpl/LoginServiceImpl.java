2cf305186e9e812655e65b2928dbccdf544e9093
Manuel <manueldeho@gmail.com>
2020-03-03 18:04:34 +0100

    Creato metodo per inviare email nel LoginServiceImpl e modificato il pom

--- biblioteca/src/main/java/serviceimpl/LoginServiceImpl.java
+++ biblioteca/src/main/java/serviceimpl/LoginServiceImpl.java
@@ -1,5 +1,14 @@
 package serviceimpl;
 
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -55,23 +64,69 @@ public class LoginServiceImpl implements LoginService {
 		}
 		return pagina;
 	}
-	
+
 	@Override
 	public void passwordDimenticata(HttpServletRequest request, Utente utente) throws Eccezione {
+		// manuel
 		String pagina = "";
+
+		// imposto la lunghezza della nuova password e la creo
 		int lunghezzaPassword = 7;
+		String nuovaPassword = RandomPassword.getPassword(lunghezzaPassword);
+
+		// recupero la lista di utenti che conterrà solo l'utente cercato per email
 		List<Utente> lista = UtenteDao.cercaUtente(utente);
-		if(lista.isEmpty()) {
+		if (lista.isEmpty()) {
 			throw new Eccezione("Email non trovata");
 		}
+
+		// Recupero l'email dell'utente trovato e preparo il messaggio
 		Utente utenteTrovato = lista.get(0);
-		
-		// recupero l'indirizzo email dell'utente nel campo di input
-		// genero la password e la inserisco nel documento da inviare all'indirizzo
-		// email dell'utente
-		String nuovaPassword = RandomPassword.getPassword(lunghezzaPassword);
-		// inserisco all'interno del documento anche il link che reindirizza alla pagina
-		// di nuova password
+		String sendTo = utenteTrovato.getEmail();
+		String sendFrom = "libreriavalueson@gmail.com";
+		String host = "smtp.gmail.com";
+
+		Properties properties = System.getProperties();
+
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// prendo l'istanza della sessione, recupero le properties e passo email e
+		// password
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication("libreriavalueson@gmail.com", "Libreria00");
+
+			}
+		});
+
+		try {
+			// creo il messaggio
+			MimeMessage message = new MimeMessage(session);
+
+			// Setta l'header del campo from
+			message.setFrom(new InternetAddress(sendFrom));
+
+			// Setta l'header del campo to
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
+
+			// Setta il campo dell'oggetto
+			message.setSubject("Campo dell'oggetto");
+
+			// Setta il messaggio vero e proprio
+			message.setText("Hai richiesto la nuova password. La nuova password è" + nuovaPassword);
+
+			System.out.println("sending...");
+			// Invia il messaggio
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
 	}
 
 	@Override
