8e797580d2d83fef246c64d28c327cc52986bd98
Manuel <manueldeho@gmail.com>
2020-03-03 12:20:13 +0100

    Aggiunta classe in utilities RandomPassword e modifiche LoginServiceImpl

--- biblioteca/src/serviceimpl/LoginServiceImpl.java
+++ biblioteca/src/serviceimpl/LoginServiceImpl.java
@@ -4,38 +4,40 @@ import javax.servlet.http.HttpServletRequest;
 
 import service.LoginService;
 import utilities.Eccezione;
+import utilities.RandomPassword;
 
-public class LoginServiceImpl implements LoginService{
-	
-	  private static LoginServiceImpl istance=null; 
-	  
-	  private LoginServiceImpl() {
-	  }
+public class LoginServiceImpl implements LoginService {
 
-	  public static LoginServiceImpl getIstance() {
-	    if(istance==null)
-	      istance = new LoginServiceImpl();
-	    return istance;
-	  }
-	    
+	private static LoginServiceImpl istance = null;
+
+	private LoginServiceImpl() {
+	}
+
+	public static LoginServiceImpl getIstance() {
+		if (istance == null)
+			istance = new LoginServiceImpl();
+		return istance;
+	}
+
 	@Override
 	public void login(HttpServletRequest request, String username, String password) throws Eccezione {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void passwordDimenticata(HttpServletRequest request, String email) throws Eccezione {
-		
-		
+		int lunghezzaPassword = 7;
+		// recupero l'indirizzo email dell'utente nel campo di input
+		// genero la password e la inserisco nel documento da inviare all'indirizzo email dell'utente
+		String nuovaPassword = RandomPassword.getPassword(lunghezzaPassword);
+		// inserisco all'interno del documento anche il link che reindirizza alla pagina di nuova password
 	}
 
 	@Override
 	public void nuovaPassword(HttpServletRequest request, String email, String password) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-		
-	}
-
+}
---
+++ biblioteca/src/utilities/RandomPassword.java
@@ -0,0 +1,30 @@
+package utilities;
+//deho
+public class RandomPassword { 
+		  
+	    // genera una password random di lunghezza n 
+	    public static String getPassword(int n) 
+	    { 
+	  
+	        // caratteri che vengono usati nello StringBuilder
+	        String caratteriUsati = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"0123456789"+"abcdefghijklmnopqrstuvxyz"+",.-_?^!$%&"; 
+	  
+	        // creo StringBuilder che prende in entrata la lunghezza n della password da generare 
+	        StringBuilder sb = new StringBuilder(n); 
+	        
+	        // itero per creare la variabile sb
+	        for (int i = 0; i < n; i++) { 
+	  
+	            // genero un numero random che varia da zero alla lunghezza della variabile caratteriUsati
+	        	// e lo assegno a index
+	            int index = (int)(caratteriUsati.length() * Math.random()); 
+	  
+	            // aggiungo i caratteri uno a uno nello StringBuilder sb
+	            // prendendo il carattere in corrispondenza del numero index generato prima
+	            sb.append(caratteriUsati.charAt(index)); 
+	        } 
+	        
+	        return sb.toString(); 
+	    } 
+
+	} 
