ed9a9ddc55dd45b4edbffa4030ed20fecb926ef7
matteo <saccone.matteo.io@gmail.com>
2020-03-03 14:36:30 +0100

    commenti utenteService

--- biblioteca/src/service/UtenteService.java
+++ biblioteca/src/service/UtenteService.java
@@ -6,6 +6,12 @@ import model.Utente;
 import utilities.Eccezione;
 
 public interface UtenteService {
+	
+	/**
+	 * permette l'inserimento di un utente nel db
+	 * @param u: Parametro di tipo utente che contiene i dati che dovranno essere inseriti nel db  
+	 * @throws Eccezione: Il metodo lancia un eccezione di tipo Eccezzione che estende Exception
+	 */
 	void createUtente(Utente u) throws Eccezione;
 
 	void updateUtente(Utente u, Long id) throws Eccezione;
